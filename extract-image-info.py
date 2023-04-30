fc17270f5c0a556f58bf89fbcffc420e0b5a3b9a
Josh Bruton <103232802+BRuDesDev@users.noreply.github.com>
2023-04-30 14:25:34 -0400

    Update extract-image-info.py

--- extract-image-info.py
+++ extract-image-info.py
@@ -1,5 +1,25 @@
 
 
+def extractimageinfo(imageFile):
+    # Opening Image as an object
+    Img = Image.open(imageFile)
+    # Getting the filename of image
+    print("Filename : ",Img.filename)
+    # Getting the format of image
+    print("Format : ",Img.format)
+    # Getting the mode of image
+    print("Mode : ",Img.mode)
+    # Getting the size of image
+    print("Size : ",Img.size)
+    # Getting only the width of image
+    print("Width : ",Img.width)
+    # Getting only the height of image
+    print("Height : ",Img.height)
+    # Getting the color palette of image
+    print("Image Palette : ",Img.palette)
+    # Getting the info about image
+    print("Image Info : ",Img.info)
+    # Closing Image object
+    Img.close()
 
-def extractimageinfo():
-    pass
+
